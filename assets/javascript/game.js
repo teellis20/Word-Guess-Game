8694450b5c3e96e7013434caa7d6be8c35e3ae6c
Tellis <teellis20@gmail.com>
2019-08-10 22:46:48 -0700

    replace with correct letters logic added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,18 +24,21 @@ var wordBank = ["saloon", "saddle", "yeehaw", "shootout"];
 var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 var userGuess;
 var blankFiller;
+var id;
 var isWrong = false;
 var strikes = 0;
 
-
 // adding the blanks and letters to the game
-function getBlanks(array) {
-    for ( i = 0; i < array.length; i++ ) {
+function getBlanks(blank,spelled) {
+    for ( i = 0; i < blank.length; i++ ) {
+      old = i;
+      id = spelled[i] + old.toString();
       blankFiller = $("<div>");
       blankFiller.addClass("blankclass");
-      blankFiller.attr("data-letter", array[i]);
-      blankFiller.text(array[i]);
+      blankFiller.attr("id", id);
+      blankFiller.text(blank[i]);
       $("#blanks").append(blankFiller);
+      console.log("first id: " + id);
     }
 };
 
@@ -46,7 +49,11 @@ console.log(randomWord);
  function checkGuess(array) {
     for (i = 0; i < array.length; i++) {
         if (userGuess == array[i]) {
+            old = i;
+            id = "#" + array[i] + old.toString();
+           $(id).html(array[i]);
             console.log("right guess");
+            console.log("your id is: " + id);
         } else {
             isWrong = true;
             console.log("wrong guess");
@@ -77,21 +84,20 @@ document.onkeyup = function(event) {
 
 // fill in the divs with the blanks/letters
 if (randomWord == "saloon") {
-    getBlanks(saloon.blank);
+    getBlanks(saloon.blank, saloon.spelledOut);
 } 
 
 if (randomWord == "saddle") {
-    getBlanks(saddle.blank);
+    getBlanks(saddle.blank, saddle.spelledOut);
 } 
 
 if (randomWord == "yeehaw") {
-    getBlanks(yeehaw.blank);
+    getBlanks(yeehaw.blank, yeehaw.spelledOut);
 } 
 
 if (randomWord == "shootout") {
-    getBlanks(shootout.blank); 
+    getBlanks(shootout.blank, shootout.spelledOut); 
 };
 
 
-// for loop with if charAt(i) to check letters guessed against word
 
